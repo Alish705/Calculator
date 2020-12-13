5685f0424d43cd2b2a0ab43dd74766c516416913
Alish705 <alishawal705@gmail.com>
2020-12-13 15:52:08 +1100

    Refactoring code and fixed bugs

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function handleOperator(nextOperator) {
     calculator.firstOperand = inputValue;
   } else if (operator) {
     const result = calculate(firstOperand, inputValue, operator);
-    calculator.displayValue = String(result);
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
     calculator.firstOperand = result;
   }
 
@@ -83,30 +83,33 @@ function updateDisplay() {
 updateDisplay();
 
 const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
+ keys.addEventListener('click', event => {
   const { target } = event;
+  const { value } = target;
   if (!target.matches('button')) {
     return;
   }
 
-  if (target.classList.contains('operator')) {
-    handleOperator(target.value);
-updateDisplay();
-    return;
+  switch (value) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '=':
+      handleOperator(value);
+      break;
+    case '.':
+      inputDecimal(value);
+      break;
+    case 'all-clear':
+      resetCalculator();
+      break;
+    default:
+      // check if the key is an integer
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
   }
 
-  if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-		updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains('all-clear')) {
-    resetCalculator();
-updateDisplay();
-    return;
-  }
-
-  inputDigit(target.value);
   updateDisplay();
 });
